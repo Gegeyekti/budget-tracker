8ef253d9595b99e3af149aae4b8ab0d007b62d04
galihyekti <153338208+Gegeyekti@users.noreply.github.com>
2026-01-20 15:00:40 +0700

    fix: railway server port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const config = require("./config/config");
 
-const PORT = config.server.port;
+const PORT = config.server.port || process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
