7bdac1b38b6f0882a9a96689624d4d1a116c9948
galihyekti <153338208+Gegeyekti@users.noreply.github.com>
2026-01-20 16:15:59 +0700

    fix: railway server port

--- backend/src/store/sequelize.js
+++ backend/src/store/sequelize.js
@@ -11,32 +11,18 @@ console.log("DB ENV CHECK =>", {
 console.log("DB USER CHECK =>", process.env.DB_USER);
 
 
-const sequelize = new Sequelize(
-  config.db.database,
-  config.db.username,
-  config.db.password,
-  {
-    host: config.db.host,
-    port: config.db.port,
-    dialect: config.db.dialect,
-    logging: false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  },
-);
+const { Sequelize } = require("sequelize");
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-    process.exit(1); // Railway akan restart service
-  }
-})();
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  logging: false,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+
 
 module.exports = sequelize;
